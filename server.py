efefe0c7e6dd0fd919151c3dbc221734b1abfe0d
Martin Christoph Frank <martin7.frank7@gmail.com>
2024-02-21 11:42:46 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,91 @@
+import fastapi
+from typing import List
+import networkx as nx
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+app = fastapi.FastAPI()
+
+# create a graph
+input_graph = nx.Graph()
+annotation_graph = nx.Graph()
+
+LLM_BASE_URL = "http://localhost:11434"
+
+
+class Link(BaseModel):
+    source: str
+    target: str
+
+
+class MessageNode(BaseModel):
+    id: str
+    name: str
+    timestamp: int
+    origin: str
+    text: str
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatCompletionRequest(BaseModel):
+    messages: List[Message]
+    model: str
+
+
+class ChatCompletionChoice(BaseModel):
+    message: Message
+
+
+class ChatCompletionResponse(BaseModel):
+    choices: List[ChatCompletionChoice]
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def read_root():
+    return fastapi.responses.FileResponse("index.html")
+
+
+def request_chat_completion(
+        request: ChatCompletionRequest
+) -> ChatCompletionResponse:
+    import requests
+
+    try:
+        response = requests.post(
+            f"{LLM_BASE_URL}/v1/chat/completions",
+            json=request.dict()
+        )
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return ChatCompletionResponse(
+            choices=[
+                ChatCompletionChoice(
+                    message=Message(
+                        role="system",
+                        content="Request failed: " + str(e)
+                    )
+                )
+            ])
+
+    return response.json()
+
+
+@app.post("/proxy/v1/chat/completions")
+def make_chat_completion(
+        request: ChatCompletionRequest
+) -> ChatCompletionResponse:
+    chat_response = request_chat_completion(request)
+    return chat_response
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=5020)
---
+++ static/script.js
@@ -0,0 +1,90 @@
+const LLM_MODEL_NAME = "tinyllama:chat"
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sendMessageButton = document.getElementById('sendMessage');
+    const inputText = document.getElementById('inputText');
+    const messages = document.getElementById('messages');
+
+    function addMessage(message){
+        const messageDiv = document.createElement('div');
+        messageDiv.className = `message ${message.role}`; // Add role class here
+
+        const roleDiv = document.createElement('div');
+        roleDiv.className = 'role';
+        roleDiv.textContent = message.role; // Display role
+
+        const contentDiv = document.createElement('div');
+        contentDiv.className = 'content';
+        contentDiv.textContent = message.content;
+
+        messageDiv.appendChild(roleDiv);
+        messageDiv.appendChild(contentDiv);
+
+        messages.appendChild(messageDiv);
+    }
+
+    sendMessageButton.addEventListener('click', function() {
+        const messageContent = inputText.value.trim();
+
+        if (messageContent) {
+            addMessage({
+                role: 'user',
+                content: messageContent
+            });
+
+            inputText.value = ''; // Clear input after sending
+
+            getResponse();
+        }
+    });
+
+    function getResponse(){
+        sendMessageButton.disabled = true; // Disable button while request is in progress
+        const messages = getMessages();
+        console.log(messages);
+
+        const response = fetch(`/proxy/v1/chat/completions`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                model: LLM_MODEL_NAME,
+                messages: messages,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Assuming the server response includes a message or messages array
+            console.log(data);
+            if (data.choices && data.choices.length > 0) {
+                addMessage(data.choices[0].message);
+            }
+        })
+        .catch(error => console.error('Error:', error))
+        .finally(() => {
+            sendMessageButton.disabled = false; // Re-enable button after request
+        });
+    }
+
+    function getMessages() {
+        const messages = [];
+
+        const messageDivs = document.querySelectorAll('.message');
+
+        //message just has role = user | system | assistant and content: str
+        
+        for (let i = 0; i < messageDivs.length; i++) {
+            const messageDiv = messageDivs[i];
+            const role = messageDiv.querySelector('.role').textContent;
+            const content = messageDiv.querySelector('.content').textContent;
+
+            messages.push({
+                role,
+                content
+            });
+        }
+
+        return messages;
+    }
+});
